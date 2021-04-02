d4938aab6f0a52225d1cbfa3d4c0a71e13ae2f06
Luki Dwi Prasanti <80494785+lukidwiprasanti@users.noreply.github.com>
2021-04-02 09:52:36 +0700

    Add files via upload

---
+++ Minggu ke-4/Tugas Minggu 4/Tugas 2 Kelipatan 2/src/tugas/pkg2/perkalian/pkg2/Tugas2Kelipatan2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.pkg2.perkalian.pkg2;
+
+/**
+ *
+ * @author ASUSPC
+ */
+public class Tugas2Kelipatan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Perkalian 2 0-100
+        System.out.println("        Do_While        ");
+        System.out.println("Bilangan Kelipatan 2 (1-100)");
+        System.out.println("============================");
+        int angka = 2;
+        int i;
+        do{
+            System.out.print(" " + angka);
+            angka = angka + 2;
+        }
+        while (angka <=100);
+    }
+    
+}
