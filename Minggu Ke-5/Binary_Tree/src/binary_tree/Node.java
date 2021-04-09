72553ebd983bfa11edcec00edfdeaf6e865ac9a1
Luki Dwi Prasanti <80494785+lukidwiprasanti@users.noreply.github.com>
2021-04-09 19:33:32 +0700

    Add files via upload

---
+++ Minggu Ke-5/Binary_Tree/src/binary_tree/Node.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binary_tree;
+
+/**
+ *
+ * @author ASUSPC
+ */
+public class Node{
+    public int id;
+    public String data;
+    public Node leftChild;
+    public Node rightChild;
+    
+    public void displayNode(){
+        System.out.print(" { " + id + ", " + data + " } ");
+    }
+    }
+    
+
